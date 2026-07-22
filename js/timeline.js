e8ec947b68c51337925c7089ce9581447e2be6d3
uzgum <diegol.enllamas@gmail.com>
2026-07-22 11:22:16 -0600

    v.0.1.3.1(cambios menores timeline)

--- js/timeline.js
+++ js/timeline.js
@@ -39,10 +39,11 @@ const historyEras = [
         quote: '"El firmamento vivo y la esencia de las constelaciones."',
         description: `Los primeros astrónomos comenzaron a observar el firmamento y descubrieron que algunas estrellas no permanecían inmóviles. Los magos afirmaban que el cielo estaba vivo, los sacerdotes pensaban que eran dioses, y los druidas aseguraban que eran espíritus. Todos estaban equivocados... y a la vez, todos tenían razón.
 
-            Durante esta época surgieron los primeros observatorios que hoy permanecen en ruinas, y con ellos nacieron los primeros círculos druídicos dedicados a descifrar las Convergencias Celestes.
+        Durante esta época surgieron los primeros observatorios que hoy permanecen en ruinas, y con ellos nacieron los primeros círculos druídicos dedicados a descifrar las Convergencias Celestes.
 
-            *Sus antiguos registros describían a los Guardianes del Firmamento: espíritus ancestrales enlazados a las constelaciones que compartían su esencia con mortales nacidos bajo alineaciones sagradas. Aunque el resto del mundo archivó esto como simple mitología, los druidas conservaron celosamente la verdad.*`
-   },
+        <em>Sus antiguos registros describían a los Guardianes del Firmamento: espíritus ancestrales enlazados a las constelaciones que compartían su esencia con mortales nacidos bajo alineaciones sagradas. Aunque el resto del mundo archivó esto como simple mitología, los druidas conservaron celosamente la verdad.</em>`
+    },
+
     {
         id: 'era-reinos',
         era: '⚔️ La Era de los Reinos',
